a788c1b5e08e6ba858617629b28568042b011705
Oli Hickie <oli@thethinkingtraveller.com>
2024-01-20 14:50:22 +0000

    Update highlight to display card data

--- app/src/views/MuseumHighlight.vue
+++ app/src/views/MuseumHighlight.vue
@@ -1,20 +1,40 @@
 <template>
-    <div class="museum-highlight">
-        <!-- Display the available information for the highlight -->
+    <div class="container">
+        <div class="museum-highlight">
+            <img src="" alt="" class="museum-highlight__image">
+            <div class="museum-highlight__content">
+                <h2 class="museum-highlight__title">
+                    {{ data.name }}
+                </h2>
+                <p class="museum-highlight__description">
+                    {{ data.description }}
+                </p>
+                <PrimaryButton class="museum-highlight__cta">
+                    Refresh image
+                </PrimaryButton>
+            </div>  
+        </div>
+        <div class="icon">
+            <slot name="icon" />
+        </div>
     </div>
 </template>
 
 <script>
+import PrimaryButton from '@/components/Base/PrimaryButton.vue';
 
 export default {
     name: 'MuseumHighlight',
     components: {
-
+        PrimaryButton,
     },
     mixins: [
     ],
     props: {
-
+        data: {
+            type: Object,
+            required: true,
+        },
     },
     data() {
         return {
@@ -36,7 +56,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.museum-highlight {
 
+.container {
+    position: relative;
+    .museum-highlight {
+        &__image {
+            width: 100%;
+            aspect-ratio: 3/2;
+            background-color: grey;
+        }
+        &__cta {
+            display: block;
+            margin: 2rem auto;
+        }
+    }
+    .icon {
+        position: absolute;
+        top: -24px;
+        right: -20px;
+    }
 }
 </style>
